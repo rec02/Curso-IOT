f847ed5fdf03f6635fc76644e5356a3b5fe9eebd
rec02 <gerardo.recinos5355@gmail.com>
2019-10-09 10:02:31 -0600

    Terminado.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
-function setup() {
-  createCanvas(640, 480);
+let BotonActivar;
+let Botonapagar;
+let EstadoFondo = false;
+
+var client = mqtt.connect('wss:JavierRecinos:1AECA1234basagei@broker.shiftr.io'; {
+  clientId:'ControladorWeb'
+})
+
+function draw (){
+  if (EstadoFondo) {
+    background(0);
+  }
+  else{
+    background (255);
+  }
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function setup (){
+  createCanvas (200,200);
+  createP();
+  BotonActivar = createButton ("ActivarLed");
+  BotonApagar = createButton ("ApagarLed");
+  BottonActivar.mousePressed (ActivarLed);
+  BottonApagar.mousePressed (ApagarLed);
+
 }
+function ApagarLed (){
+  console.log ("Apagar Led");
+  client.publish ('/JavierRecinos/Led', '1');
+
+}
+
+function ActivarLed (){
+  console.log ("Encender Led");
+  client.publish ('/JavierRecinos/Led','1');
+}
+
+client.on ('connect', function (){
+  console.log('MQTT conectado');
+  client.suscribe ('/JavierRecinos/Boton');
+}
+);
